58909be6503051b874fb1fe5e8a06683a10d7534
aux928 <tacooly@yahoo.com.tw>
2016-03-14 12:59:44 +0800

    修改成 顯示Free或Thank you!

--- app/src/main/java/com/example/student119/lab02_01_interactive/MainActivity.java
+++ app/src/main/java/com/example/student119/lab02_01_interactive/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+    private int mQuantity = 0;
+    private int mPrice = 5 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,19 +17,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void submitOrder(View view) {
-        display(2);
+
+
+    public  void submitOrder(View view) {
+        displayTotalPrice();
+    }
+    private void displayQuantity() {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(String.valueOf(mQuantity));
     }
 
-    private void display(int number) {
-        TextView quantityTextView =(TextView)findViewById(R.id.quantity_text_view);
-        quantityTextView.setText(String.valueOf(number));
-
-        TextView priceTextView =(TextView)findViewById(R.id.price_text_view);
-        int price = 5;
-        int total = price * number;
-        String myString = NumberFormat.getCurrencyInstance().format(total);
+    private void displayTotalPrice(){
+        TextView priceTextView = (TextView)findViewById(R.id.price_text_view);
+        int total = mPrice * mQuantity;
+        String myString= NumberFormat.getCurrencyInstance().format(total);
+        String message = myString + (mQuantity ==0 ? "\nFree" : "\nThank you !");
         priceTextView.setText(myString);
+    }
+    public void increment(View view){
+        ++mQuantity;
+        displayQuantity();
+        resetTotalPrice();
+    }
+
+    public void decrement(View view) {
+        if (mQuantity > 0) {
+            --mQuantity;
+            displayQuantity();
+            resetTotalPrice();
+        }
+
+    }
+    private void  resetTotalPrice() {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText("");
     }
 }
 
